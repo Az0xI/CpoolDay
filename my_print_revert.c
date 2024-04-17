678f0771434d3e9ca5648d339dd8ded8e12b4cec
Az0xI <118692569+Az0xI@users.noreply.github.com>
2024-04-17 12:19:46 +0000

    pas fini

---
+++ my_print_revert.c
@@ -0,0 +1,21 @@
+void print_char(char c);
+int length(char *chaine);
+
+void my_print_revert(char *toRevert)
+{   
+    int i = length(toRevert);
+    for(i ; i = 0; i--){
+    print_char(toRevert[i]);
+    }
+}
+
+
+
+int length(char *chaine)
+{
+    int iterateur=0;
+    while(chaine[iterateur] != '\0') { 
+    iterateur++;
+    }
+return iterateur;
+}
