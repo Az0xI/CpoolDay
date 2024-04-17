5c026b2e931cd8743b5f7c03eb5679a00774030f
Az0xI <118692569+Az0xI@users.noreply.github.com>
2024-04-17 12:33:47 +0000

    presque bon

--- my_print_revert.c
+++ my_print_revert.c
@@ -4,14 +4,14 @@ int length(char *chaine);
 void my_print_revert(char *toRevert)
 {   
     int i = length(toRevert);
-    for(i ; i = 0; i--){
+    for(i ; i >= 0; i--){
     print_char(toRevert[i]);
     }
 }
 
 
 
-int length(char *chaine)
+int length(char chaine[])
 {
     int iterateur=0;
     while(chaine[iterateur] != '\0') { 
