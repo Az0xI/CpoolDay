22886d9a131c757bd227025562ddf46360d6ad52
Az0xI <118692569+Az0xI@users.noreply.github.com>
2024-04-17 10:05:36 +0000

    pitié faite que sa passe la mouli ;-;

---
+++ my_print_n_ascii.c
@@ -0,0 +1,12 @@
+void print_char(char c);
+
+void my_print_n_ascii(int HowMany)
+{
+    if ((33 + HowMany) < 127)
+    {
+        for(int compteur = 32 ; compteur < (33 + HowMany) ; compteur++)
+        {
+            print_char(compteur);
+        }
+    }
+}
